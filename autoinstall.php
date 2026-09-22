f24843576265a71d0b0d474e414374856c208940
Ben <hostellerie.org@gmail.com>
2026-09-22 11:40:03 +0700

    Prepare Cookie Consent 1.3 configuration and metadata

--- autoinstall.php
+++ autoinstall.php
@@ -14,7 +14,7 @@ function plugin_autoinstall_cookieconsent($pi_name)
         'info' => array(
             'pi_name' => $piName,
             'pi_display_name' => $displayName,
-            'pi_version' => '1.1.0',
+            'pi_version' => '1.3.0',
             'pi_gl_version' => '2.1.1',
             'pi_homepage' => 'https://github.com/hostellerie/cookieconsent'
         ),
@@ -36,12 +36,15 @@ function plugin_autoinstall_cookieconsent($pi_name)
 function plugin_load_configuration_cookieconsent($pi_name)
 {
     global $_CONF;
+
     $defaults = $_CONF['path'] . 'plugins/' . $pi_name . '/install_defaults.php';
     if (!file_exists($defaults)) {
         return false;
     }
+
     require_once $_CONF['path_system'] . 'classes/config.class.php';
     require_once $defaults;
+
     return function_exists('plugin_initconfig_cookieconsent') && plugin_initconfig_cookieconsent();
 }
 
@@ -50,6 +53,7 @@ function plugin_compatible_with_this_version_cookieconsent($pi_name)
     if (!defined('VERSION')) {
         return false;
     }
+
     if (function_exists('COM_versionCompare')) {
         if (!COM_versionCompare(VERSION, '2.1.1', '>=')) {
             return false;
@@ -57,5 +61,6 @@ function plugin_compatible_with_this_version_cookieconsent($pi_name)
     } elseif (version_compare(VERSION, '2.1.1', '<')) {
         return false;
     }
+
     return version_compare(PHP_VERSION, '5.6.0', '>=');
 }
